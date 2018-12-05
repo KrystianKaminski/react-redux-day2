a7ba0c93ef3e7212c0d465fbbbe30929da8fbc64
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-12-05 10:48:39 +0100

    Task finished

--- src/state/counter.js
+++ src/state/counter.js
@@ -5,10 +5,25 @@ const INITIAL_STATE = {
     number: 0
 }
 
+export const increment = () => ({
+    type: INC
+})
+
+export const decrement = () => ({
+    type: DEC
+})
+
+
 export default (state = INITIAL_STATE, action) => {
     switch(action.type) {
-
-
+        case INC:
+            return {
+                number: state.number + 1
+            }
+        case DEC:
+            return {
+                number: state.number - 1
+            }
         default:
             return state
     }
